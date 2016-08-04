c93be3d33a496865884c60e0a092c7cf6e4c4046
Antti Kuosmanen <antti@seravo.fi>
2016-08-04 21:48:18 +0300

    Modifications for seravo.com design

--- generator.php
+++ generator.php
@@ -4,7 +4,7 @@
  */
 
 $config = array(
-	'footer' => 'Technical Contact: <a href="mailto:x@example.com">x@example.com</a>'
+	'footer' => '<a href="https://seravo.com">seravo.com</a> | <a href="https://wp-palvelu.fi">wp-palvelu.fi</a>'
 );
 
 // load pages
@@ -15,17 +15,17 @@ $css = trim(file_get_contents('Resources/Layout.css'));
 
 // generate each error page
 foreach ($pages as $code => $page){
-	
+
 	// assign variables
-	$v_code = intval($code);
+	$v_code = $code;
 	$v_title = nl2br(htmlspecialchars($page['title']));
-	$v_message = nl2br(htmlspecialchars($page['message']));	
-	
+	$v_message = nl2br(htmlspecialchars($page['message']));
+
 	// render template
 	ob_start();
 	require('template.phtml');
 	$errorpage = ob_get_clean();
-	
+
 	// store template
-	file_put_contents('Build/HTTP'.$v_code.'.html', $errorpage);
+	file_put_contents('Build/'.$v_code.'.html', $errorpage);
 }
--- pages.php
+++ pages.php
@@ -1,66 +1,52 @@
 <?php
 return array (
-		
-		'400' => array (
-				'title' => 'Bad Request',
-				'message' => 'The server cannot process the request due to something that is perceived to be a client error.' 
-		),
-		'401' => array (
-				'title' => 'Unauthorized',
-				'message' => 'The requested resource requires an authentication.'
-		),
+    "400" => array (
+        "title" => "Bad Request",
+        "message" => "Something went wrong with your request. Please go back."
+    ),
 
-		'403' => array (
-				'title' => 'Access Denied',
-				'message' => 'The requested resource requires an authentication.' 
-		),
-		
-		// http 404 not found
-		'404' => array (
-				'title' => 'Resource not found',
-				'message' => 'The requested resource could not be found but may be available again in the future.' 
-		),
-		
-		// internal server error
-		'500' => array (
-				'title' => 'Webservice currently unavailable',
-				'message' => "An unexpected condition was encountered.\nOur service team has been dispatched to bring it back online." 
-		),
-		
-		// unknown http method
-		'501' => array (
-				'title' => 'Not Implemented',
-				'message' => 'The Webserver cannot recognize the request method.'
-		),
-		
-		// http proxy forward error
-		'502' => array (
-				'title' => 'Webservice currently unavailable',
-				'message' => "We've got some trouble with our backend upstream cluster.\nOur service team has been dispatched to bring it back online."
-		),
-		
-		// webserver service error
-		'503' => array (
-				'title' => 'Webservice currently unavailable',
-				'message' => "We've got some trouble with our backend upstream cluster.\nOur service team has been dispatched to bring it back online."
-		),
-		
-		// webserver origin error
-		'520' => array(
-			'title' => 'Origin Error - Unknown Host',
-			'message' => 'The requested hostname is not routed. Use only hostnames to access resources.'
-		),
-		
-		// webserver down error
-		'521' => array (
-				'title' => 'Webservice currently unavailable',
-				'message' => "We've got some trouble with our backend upstream cluster.\nOur service team has been dispatched to bring it back online."
-		),
-		
-		// maintenance
-		'533' => array(
-				'title' => 'Scheduled Maintenance',
-				'message' => "This site is currently down for maintenance.\nOur service team is working hard to bring it back online soon."				
-		)
-)
-;
+    "403" => array (
+        "title" => "Access Denied",
+        "message" => "Your request has been blocked. Please contact the site's owner."
+    ),
+
+    "404" => array (
+        "title" => "Not Found",
+        "message" => "The page you requested seems to be missing. "
+    ),
+
+    "413" => array (
+        "title" => "Request Entity Too Large",
+        "message" => "It looks like you tried to upload something really big and the site couldn't handle it."
+    ),
+
+    "414" => array (
+        "title" => "Request URI Too Long",
+        "message" => "The address of the page you tried to reach was too long. Please contact the site's owner."
+    ),
+
+    "444" => array (
+        "title" => "Access Denied",
+        "message" => "Your request has been blocked. Please contact the site's owner."
+    ),
+
+    "500" => array (
+        "title" => "Unexpected Error",
+        "message" => "Please contact the site's owner about this error. If you are the owner, check your logs for details."
+    ),
+
+    "502" => array (
+        "title" => "Service currently unavailable",
+        "message" => "Our servers seem to be having some trouble. Please try again later."
+    ),
+
+    "504" => array (
+        "title" => "Service currently unavailable",
+        "message" => "Our servers seem to be having some trouble. Please try again later."
+    ),
+
+    "index" => array (
+        "title" => "Not Found",
+        "message" => "The page you requested seems to be missing. "
+    ),
+);
