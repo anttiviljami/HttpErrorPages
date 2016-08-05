3091cfb4558fb715de05a931a8cacfa02c44cef7
Antti Kuosmanen <antti@seravo.fi>
2016-08-05 10:35:09 +0300

    Add Finnish texts. Sanna's modifications.

--- generator.php
+++ generator.php
@@ -4,7 +4,8 @@
  */
 
 $config = array(
-	'footer' => '<a href="https://seravo.com">seravo.com</a> | <a href="https://wp-palvelu.fi">wp-palvelu.fi</a>'
+  'footer' => null,
+///	'footer' => '<a href="https://seravo.com">seravo.com</a> | <a href="https://wp-palvelu.fi">wp-palvelu.fi</a>'
 );
 
 // load pages
@@ -19,7 +20,8 @@ foreach ($pages as $code => $page){
 	// assign variables
 	$v_code = $code;
 	$v_title = nl2br(htmlspecialchars($page['title']));
-	$v_message = nl2br(htmlspecialchars($page['message']));
+	$v_message0 = nl2br(htmlspecialchars($page['message0']));
+	$v_message1 = nl2br(htmlspecialchars($page['message1']));
 
 	// render template
 	ob_start();
--- pages.php
+++ pages.php
@@ -2,51 +2,61 @@
 return array (
     "400" => array (
         "title" => "Bad Request",
-        "message" => "Something went wrong with your request. Please go back."
+        "message0" => "Something went wrong with your request. Please go back and try again.",
+        "message1" => "Jotakin meni pieleen. Tarkista syöttämäsi osoite ja yritä uudelleen.",
     ),
 
     "403" => array (
         "title" => "Access Denied",
-        "message" => "Your request has been blocked. Please contact the site's owner."
+        "message0" => "Your request has been blocked. Please contact the site's owner.",
+        "message1" => "Pääsysi sivulle on estetty. Ota yhteyttä sivuston omistajaan.",
     ),
 
     "404" => array (
         "title" => "Not Found",
-        "message" => "The page you requested seems to be missing. "
+        "message0" => "The page you requested seems to be missing. Please check the address and try again.",
+        "message1" => "Etsimääsi sivua ei löytynyt. Tarkista sivun osoite ja yritä uudelleen.",
     ),
 
     "413" => array (
         "title" => "Request Entity Too Large",
-        "message" => "It looks like you tried to upload something really big and the site couldn't handle it."
+        "message0" => "It looks like you tried to upload something really big and the site couldn't handle it.",
+        "message1" => "Näyttää siltä, että lataamasi tiedosto on liian suuri sivustolle.",
     ),
 
     "414" => array (
         "title" => "Request URI Too Long",
-        "message" => "The address of the page you tried to reach was too long. Please contact the site's owner."
+        "message0" => "The address of the page you tried to reach was too long. Please contact the site's owner.",
+        "message1" => "Sivun osoite on liian pitkä. Ota yhteyttä sivuston omistajaan.",
     ),
 
     "444" => array (
         "title" => "Access Denied",
-        "message" => "Your request has been blocked. Please contact the site's owner."
+        "message0" => "Your request has been blocked. Please contact the site's owner.",
+        "message1" => "Pääsysi sivulle on estetty. Ota yhteyttä sivuston omistajaan.",
     ),
 
     "500" => array (
         "title" => "Unexpected Error",
-        "message" => "Please contact the site's owner about this error. If you are the owner, check your logs for details."
+        "message0" => "Please contact the site's owner about this error. If you are the owner, check your logs for details.",
+        "message1" => "Sivustolla tapahtui odottamaton virhe. Ota yhteyttä sivuston omistajaan tai katso lisätiedot lokeista.",
     ),
 
     "502" => array (
         "title" => "Service currently unavailable",
-        "message" => "Our servers seem to be having some trouble. Please try again later."
+        "message0" => "Our servers seem to be having some trouble. We are fixing the problem, please try again later.",
+        "message1" => "Sivuston palvelimella on häiriö, jota korjataan parhaillaan. Kokeile hetken kuluttua uudelleen.",
     ),
 
     "504" => array (
         "title" => "Service currently unavailable",
-        "message" => "Our servers seem to be having some trouble. Please try again later."
+        "message0" => "Our servers seem to be having some trouble. We are fixing the problem, please try again later.",
+        "message1" => "Sivuston palvelimella on häiriö, jota korjataan parhaillaan. Kokeile hetken kuluttua uudelleen.",
     ),
 
     "index" => array (
         "title" => "Not Found",
-        "message" => "The page you requested seems to be missing. "
+        "message0" => "The page you requested seems to be missing. Please check the address and try again.",
+        "message1" => "Etsimääsi sivua ei löytynyt. Tarkista sivun osoite ja yritä uudelleen.",
     ),
 );
